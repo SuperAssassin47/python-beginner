9bc6982e502894518b3a8075260b003481af8a02
Falloutsurvivor47 <79471951+SuperAssassin47@users.noreply.github.com>
2023-06-15 16:38:41 +0100

    Update alert system.py
    Python script for an Alert System
    
    **DISCLAIMER** This Alert System does NOT send an alert notification to any mobile device. It just shows a messagebox that alerts the user of a message they set it to.

--- alert system.py
+++ alert system.py
@@ -1,16 +1,26 @@
-import tkinter
-from tkinter import *
-from tkinter import messagebox as mb
-import pyautogui
-root = Tk()
-root.geometry('500x500')
-root.title('Alert System')
-root.resizable(width=FALSE, height=FALSE)
+#Import the necessary modules ########
+import tkinter                       #
+from tkinter import *                #
+from tkinter import messagebox as mb #
+import pyautogui                     #
+######################################
 
+#The main crux of the application #########
+root = Tk()                               #
+root.geometry('500x500')                  #
+root.title('Alert System')                #
+root.resizable(width=FALSE, height=FALSE) #
+###########################################
+
+#The title on the client area of the application
 Label(root, text='Alert System', font='Helvetica 20 bold').place(x=160, y=10)
 
+#Function for the "Send Alert" button
 def send_alert():
-    mb.showerror('Alert', 'NUKE INBOUND! TAKE SHELTER IMMEDIATELY!')
+    mb.showinfo('Success', "I'm an alert")
 
+#Button to "Send Alert" (output a messagebox alerting the user of a message)
 Button(root, text='Send Alert', font='arial 14 bold', bg='Red', command=send_alert).place(x=187, y=130)
+
+#Start the GUI
 root.mainloop()
