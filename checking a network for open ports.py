e9a218c02c10958698e575dd253d8c63acaef2d8
Falloutsurvivor47 <79471951+SuperAssassin47@users.noreply.github.com>
2023-06-22 14:34:20 +0100

    Update checking a network for open ports.py

--- checking a network for open ports.py
+++ checking a network for open ports.py
@@ -1,7 +1,7 @@
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-result = s.connect_ex(('192.168.27.36', 100))
+result = s.connect_ex(('localhost', 100))
 if result == 100:
     print('PORT IS OPEN')
 else:
